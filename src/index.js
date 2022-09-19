42533e1854394e8e8437ebb31b9e9ea4f3f14bdb
AdrianMiftahulH <adrianmiftahul@gmail.com>
2022-09-19 11:10:48 +0700

    Menginstall React.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navigasi from './routes/partials/nav';
 
+// === cara 1 ===
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    < Navigasi />
     <App />
   </React.StrictMode>
 );
 
+// === cara 2 ===
+// function renderDOM(content, id){
+//   ReactDOM.createRoot(content, document.getElementById('id'));
+// }
+
+// const element = <h1>Adrian Miftahul Haq</h1>
+
+// renderDOM(<app />, "root");
+// renderDOM(<nav />, "nav");
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
